5b27f2ab13040458ec179758be8edb389a13a8ae
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-03-31 16:11:26 -0400

    Create object to store invariant types.

---
+++ src/Members/InvariantTypes/InvariantTypes.js
@@ -0,0 +1,8 @@
+import { MinLengthInvariant } from "./MinLengthInvariant";
+
+let INVARIANT_TYPES = {
+    MIN_LENGTH: MinLengthInvariant,
+};
+INVARIANT_TYPES = Object.freeze(INVARIANT_TYPES);
+
+export default INVARIANT_TYPES;
