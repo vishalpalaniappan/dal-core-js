3a44d932518da3a8f955a47e53fc50bab52cd4f2
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-03 15:53:42 -0400

    Fix test name.

--- tests/Implementation.test.js
+++ tests/Implementation.test.js
@@ -45,7 +45,7 @@ describe("Implementation tests", () => {
         expect(d.getFiles().length).toBe(0);
     });
 
-    it("adds and removes source files", async () => {
+    it("implementation serialization and deserialization", async () => {
         let d = new DALEngine({name: "Library Manager", description: "Manages the library"});
         d.addFile("A", "Test File", "This is a test file.");
         expect(d.getFile("A")).toBeDefined();
