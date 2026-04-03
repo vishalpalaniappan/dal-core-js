da526c67e4f33c4ee1f8ea1f9f91779ef2688d6c
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-03 09:08:18 -0400

    Add docs for methods and add methods to set and get mapping for a file.

--- src/Implementation.js
+++ src/Implementation.js
@@ -20,6 +20,8 @@ export class Implementation {
      * @param {String} name Name of the source file.
      * @param {String} path Path of the source file.
      * @param {String} content Content of the source file.
+     * @throws {Error} Throws an error if a source file with the given path
+     * already exists in the implementation.
      */
     addSourceFile (name, path, content) {
         const existingFile = this.getSourceFile(path);
@@ -33,12 +35,52 @@ export class Implementation {
         });
     }
 
+    /**
+     * Gets the source file given a path.
+     * @param {String} path Path of the source file to get.
+     * @returns {Object} The source file with the given path.
+     */
     getSourceFile (path) {
         return this._sourceFiles.find(file => file.path === path);
     }
 
+    /**
+     * Get all the source files in the implementation.
+     * @returns {Array} List of source files in the implementation.
+     */
     getSourceFiles () {
         return this._sourceFiles;
+    }
+
+    /**
+     * Sets the mapping for a source file.
+     * @param {String} path Path of the source file.
+     * @param {Object} mapping Mapping to set for the source file.
+     * @throws {Error} Throws an error if the source file with the
+     * given path does not exist in the implementation.
+     */
+    setMapping (path, mapping) {
+        const sourceFile = this.getSourceFile(path);
+        if (!sourceFile) {
+            throw new Error(`Source file with path ${path} does not exist in the implementation.`);
+        }
+        sourceFile.mapping = mapping;
+    }
+
+
+    /**
+     * Gets the mapping for a source file.
+     * @param {String} path Path of the source file.
+     * @returns {Object} The mapping of the source file.
+     * @throws {Error} Throws an error if the source file with the
+     * given path does not exist in the implementation.
+     */
+    getMapping (path) {
+        const sourceFile = this.getSourceFile(path);
+        if (!sourceFile) {
+            throw new Error(`Source file with path ${path} does not exist in the implementation.`);
+        }
+        return sourceFile.mapping;
     }
 
 }
