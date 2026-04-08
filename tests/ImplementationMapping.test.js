bc666568debb854248bfcdbf5879738d11874851
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-08 19:06:47 -0400

    Add source file into implementation.

--- tests/ImplementationMapping.test.js
+++ tests/ImplementationMapping.test.js
@@ -1,8 +1,8 @@
 import {readFile, unlink, writeFile} from "fs/promises"
 import {resolve} from "path"
-import { describe, expect, it } from "vitest";
+import {describe, expect, it} from "vitest";
 
-import { DALEngine } from "../src/DALEngine.js";
+import Implementation from "../src/Implementation/Implementation.js";
 
 describe("Implementation Mapping Tests", () => {
 
@@ -16,5 +16,10 @@ describe("Implementation Mapping Tests", () => {
         //   a behavior, or variable to it.
         // This test verifies that the statement object is created and that
         // it creates a map for each statement in the source file.
+
+        const imp = new Implementation();
+        const filePath = resolve(__dirname, "./test_data/TransactionDB.py")
+        const source = await readFile(filePath, "utf-8")
+        imp.addSourceFile("sample file", "sampleFile.py", source);
     });
 });
