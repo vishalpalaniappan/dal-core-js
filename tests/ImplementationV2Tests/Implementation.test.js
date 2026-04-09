b25d13e0ee9fba472a5348fa6b70f02dd20a831c
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-09 13:58:14 -0400

    Add tests to remove file, add stmtindex and add behavior to stmt in file.

--- tests/ImplementationV2Tests/Implementation.test.js
+++ tests/ImplementationV2Tests/Implementation.test.js
@@ -26,4 +26,46 @@ describe("implementation tests", () => {
         expect(retrievedFile).toBeDefined();
         expect(retrievedFile.getPath()).toEqual(filePath);
     });
+
+    it("removes file from implementation and verifies it is removed", async () => {
+        const {source, indexJson, filePath} = await getFiles();
+
+        const imp = new ImplementationV2();
+        const f = imp.addFile("TransactionDB.py", filePath, source);
+
+        imp.removeFile(f._uid);
+        expect(() => imp.getFile(f._uid))
+            .toThrow(`File with uid ${f._uid} not found in implementation.`);
+
+    });
+
+    it("adds file to implementation and adds statement index", async () => {
+        const {source, indexJson, filePath} = await getFiles();
+
+        const imp = new ImplementationV2();
+        const f = imp.addFile("TransactionDB.py", filePath, source);
+        imp.setStatementIndexForFile(f._uid, indexJson);
+
+        const retrievedFile = imp.getFile(f._uid);
+        const statementIndex = retrievedFile.getStatementIndex();
+        statementIndex.forEach((stmt, idx) => {
+            expect(stmt._uid).toEqual(indexJson[idx].uid);
+        });
+    });
+
+    it("adds behavior to stmtid of file and throws on invalid stmtid", async () => {
+        const {source, indexJson, filePath} = await getFiles();
+
+        const stmt1 = indexJson[0];
+
+        const imp = new ImplementationV2();
+        const f = imp.addFile("TransactionDB.py", filePath, source);
+        imp.setStatementIndexForFile(f._uid, indexJson);
+
+        f.setBehaviorId(stmt1.uid, "behavior1");
+        expect(f.getMappedStatement(stmt1.uid).getBehavior()).toBe("behavior1");
+
+        expect(() => f.setBehaviorId("asdf", "behavior1"))
+            .toThrow("Statement with ID asdf does not exist in the active version.");
+    });
 });
