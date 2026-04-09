ac0b5dcf8b584af378aa2f41db9567f724314ea8
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-09 15:40:13 -0400

    Add comments for save and reload test of implementation v2.

--- tests/ImplementationV2Tests/SaveAndReload.test.js
+++ tests/ImplementationV2Tests/SaveAndReload.test.js
@@ -2,8 +2,7 @@ import {readFile, unlink, writeFile} from "fs/promises"
 import {resolve} from "path"
 import {describe, expect, it} from "vitest";
 
-import ImplementationV2 from "../../src/Implementation/ImplementationV2";
-import { DALEngine } from "../../src/DALEngine";
+import {DALEngine} from "../../src/DALEngine";
 
 const getFiles = async () => {
     const filePath = resolve(__dirname, "../test_data/TransactionDB.py")
@@ -14,32 +13,42 @@ const getFiles = async () => {
     return {source, indexJson, filePath};
 }
 
-
 describe("serialization and deserialization", () => {
 
     it ("serializes and deserializes an implementation correctly", async () => {
         const {source, indexJson, filePath} = await getFiles();
 
         const shortIndex = [indexJson[0]];
+        let d = new DALEngine({name: "", description: ""});
 
-        let d = new DALEngine({
-            name: "Test Implementation",
-            description: "This is a test implementation.",
-        });
-
+        // Add source, statement index and behavior to statement index
         const f = d.addFileV2("src/test.py", "test.py", "SOURCE");
         f.addStatementIndex(shortIndex);
+        f.setBehavior(shortIndex[0].uid, "test behavi3or");
 
+        // For the same stmt index, set the participant and variable name
+        f.setParticipant(shortIndex[0].uid, "test participant", "variableName");
+
+        // Serialize and deseralize
         const tempFilePath = resolve(__dirname, "../temp/v2implementation.json")
         await writeFile(tempFilePath, d.serialize())
         d = new DALEngine({name: "Library Manager", description: "Manages the library"});
         d.deserialize(await readFile(tempFilePath, "utf-8"));
 
-        console.log(d.implementationV2.getFiles());
+        // Check that there is one fil
         expect(d.implementationV2.getFiles().length).toBe(1);
+
+        // Check that the first file has the correct key
         const retrievedFile = d.implementationV2.getFiles()[0];
         expect(retrievedFile.getKey()).toEqual("src/test.py");
 
+        // Get the stmt from the file and check that its behavior is what was set
+        const stmt = retrievedFile.getMappedStatement(shortIndex[0].uid);
+        expect(stmt.getBehavior()).toEqual("test behavi3or");
+
+        // Get the participant from the stmt and check that the variable name is correct
+        const participant = retrievedFile.getParticipant(shortIndex[0].uid, "test participant");
+        expect(participant.variableName).toEqual("variableName");
     });
 
 
