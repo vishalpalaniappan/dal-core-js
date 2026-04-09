0fda7c1b388d48917da903f4d6d7f3203c684433
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-09 14:34:11 -0400

    Fix source tests.

--- tests/ImplementationV2Tests/Source.test.js
+++ tests/ImplementationV2Tests/Source.test.js
@@ -2,19 +2,23 @@ import {readFile, unlink, writeFile} from "fs/promises"
 import {resolve} from "path"
 import {describe, expect, it} from "vitest";
 
-import Map from "../src/Implementation/Map";
-import Source from "../src/Implementation/Source";
+import Map from "../../src/Implementation/Map";
+import Source from "../../src/Implementation/Source";
+
+const getFiles = async () => {
+    const filePath = resolve(__dirname, "../test_data/TransactionDB.py")
+    const source = await readFile(filePath, "utf-8");
+    const mapPath = resolve(__dirname, "../test_data/TransactionDB_mapping.json");
+    const index = await readFile(mapPath, "utf-8");
+    const indexJson = JSON.parse(index);
+    return {source, indexJson, filePath};
+}
 
 describe("source tests", () => {
 
     it("creates a source object, sets content and statement index", async () => {
         // Load the source file and mapping to be used for testing
-        const filePath = resolve(__dirname, "./test_data/TransactionDB.py")
-        const source = await readFile(filePath, "utf-8")
-        const mapPath = resolve(__dirname, "./test_data/TransactionDB_mapping.json");
-        const index = await readFile(mapPath, "utf-8");
-
-        const indexJson = JSON.parse(index);
+        const {source, indexJson, filePath} = await getFiles();
 
         // Create a source object with the required attributes
         const s = new Source({uid: "source1"});
