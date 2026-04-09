4381cf3d8fc45a5098a4dfde299b7a7e94f55002
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-09 08:18:57 -0400

    Add source and simple source test.

---
+++ src/Implementation/Source.js
@@ -0,0 +1,80 @@
+import isLoadedFromFile from "../helpers/isLoadedFromFile";
+import Map from "./Map";
+
+export default class Source {
+    /**
+     * The source object represents a version of a source file in the
+     * implementation. It is represented by the content of the source file,
+     * a version ID and a statement index that maps statements in the source
+     * file to the design. Each entry in the statement index is represented
+     * by a map object that contains the behavior ID and the participants.
+     * @param {Object} args The arguments to initialize the source object with.
+     */
+    constructor (args) {
+        this._versionId = null;
+        this._content = null;
+        this._statementIndex = [];
+        (isLoadedFromFile(args) ? this._loadFromFile(args) : this._loadArgs(args));
+    }
+
+    /**
+     * Loads the invariant from the provided arguments.
+     * @throws {MissingAttributes} Thrown when required attr is not present.
+     * @param {Object} args The arguments to initialize the invariant with.
+     */
+    _loadArgs (args) {
+        const expectedAttributes = ["uid"];
+        if (typeof args !== "object" || args === null || Array.isArray(args)) {
+            // Not an object, so all attributes are missing.
+            throw new MissingAttributes("Source", expectedAttributes);
+        }
+        expectedAttributes.forEach((attr) => {
+            if (!(attr in args)) {
+                throw new MissingAttributes("Source", attr);
+            }
+            this["_" + attr] = args[attr];
+        });
+    }
+
+    _loadFromFile (mapJSON) {
+        for (const [key, value] of Object.entries(mapJSON)) {
+            this[key] = value;
+        };
+    }
+
+    /**
+     * Adds entries to the statement index.
+     * @param {Array} statementIndex The array of entries to add to the
+     * statement index.
+     */
+    addStatementIndex (statementIndex) {
+        for (const value of statementIndex) {
+            this._statementIndex.push(new Map(value));
+        };
+    }
+
+    getStatementIndex () {
+        return this._statementIndex;
+    }
+
+    getStatementIndexEntryByUid (uid) {
+        return this._statementIndex.find(entry => entry.getUid() === uid);
+    }
+
+    setVersionId (versionId) {
+        this._versionId = versionId;
+    }
+
+    getVersionId () {
+        return this._versionId;
+    }
+
+    setContent (content) {
+        this._content = content;
+        this._lastModified = new Date();
+    }
+
+    getContent () {
+        return this._content;
+    }
+};
---
+++ tests/Source.test.js
@@ -0,0 +1,40 @@
+import {readFile, unlink, writeFile} from "fs/promises"
+import {resolve} from "path"
+import {describe, expect, it} from "vitest";
+
+import Map from "../src/Implementation/Map";
+import Source from "../src/Implementation/Source";
+
+describe("source tests", () => {
+
+    it("creates a source object, sets content and statement index", async () => {
+        // Load the source file and mapping to be used for testing
+        const filePath = resolve(__dirname, "./test_data/TransactionDB.py")
+        const source = await readFile(filePath, "utf-8")
+        const mapPath = resolve(__dirname, "./test_data/TransactionDB_mapping.json");
+        const index = await readFile(mapPath, "utf-8");
+
+        const indexJson = JSON.parse(index);
+
+        // Create a source object with the required attributes
+        const s = new Source({uid: "source1"});
+
+        // Set the content and statement index of the source object
+        s.setContent(source);
+        s.addStatementIndex(indexJson);
+
+        // Get the content and statement index and verify
+        // their shape and content
+        const content = s.getContent();
+        const statementIndex = s.getStatementIndex();
+
+        expect(content).toBe(source);
+        expect(statementIndex).toBeDefined();
+        expect(statementIndex).toBeInstanceOf(Object);
+
+        const entry = s.getStatementIndexEntryByUid(indexJson[0].uid);
+        expect(entry).toBeDefined();
+        expect(entry).toBeInstanceOf(Map);
+    });
+
+});
