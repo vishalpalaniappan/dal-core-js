dd11a69f2a80ca158326e87b0f1f805ac5402422
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-09 08:09:59 -0400

    Setup map class and necessary tests.

--- src/Implementation/Map.js
+++ src/Implementation/Map.js
@@ -1,25 +1,47 @@
+import isLoadedFromFile from "../helpers/isLoadedFromFile";
+
 export default class Map {
-    constructor () {
+    constructor (args) {
         this._behaviorId = null;
         this._participants = [];
+        (isLoadedFromFile(args) ? this._loadFromFile(args) : this._loadArgs(args));
     }
 
-    setBehavior (behaviorId) {
-        this._behaviorId = behaviorId;
+    /**
+     * Loads the invariant from the provided arguments.
+     * @throws {MissingAttributes} Thrown when required attr is not present.
+     * @param {Object} args The arguments to initialize the invariant with.
+     */
+    _loadArgs (args) {
+        const expectedAttributes = ["uid", "start_line", "end_line", "source"];
+        if (typeof args !== "object" || args === null || Array.isArray(args)) {
+            // Not an object, so all attributes are missing.
+            throw new MissingAttributes("Map", expectedAttributes);
+        }
+        expectedAttributes.forEach((attr) => {
+            if (!(attr in args)) {
+                throw new MissingAttributes("Map", attr);
+            }
+            this["_" + attr] = args[attr];
+        });
+    }
+
+    _loadFromFile (mapJSON) {
+        for (const [key, value] of Object.entries(mapJSON)) {
+            this[key] = value;
+        };
     }
 
     getBehavior () {
         return this._behaviorId;
     }
 
-    setParticipant (participantName, variableName) {
-        this._participants.push({ participantName, variableName });
+    setBehavior (behaviorId) {
+        this._behaviorId = behaviorId;
     }
 
-    getParticipantByName (participantName) {
-        return this._participants.find(
-            participant => participant.participantName === participantName
-        );
+    getParticipants () {
+        return this._participants;
     }
 
     getParticipantByVariable (variableName) {
@@ -28,8 +50,37 @@ export default class Map {
         );
     }
 
-    getParticipants () {
-        return this._participants;
+    getParticipantByName (participantName) {
+        return this._participants.find(
+            participant => participant.participantName === participantName
+        );
     }
 
+    setParticipant (participantName, variableName) {
+        this._participants.push({ participantName, variableName });
+    }
+
+    getLineRange () {
+        return {startLine: this._start_line, endLine: this._end_line};
+    }
+
+    getUid () {
+        return this._uid;
+    }
+
+    removeBehavior () {
+        this._behaviorId = null;
+    }
+
+    removeParticipantByName (participantName) {
+        this._participants = this._participants.filter(
+            participant => participant.participantName !== participantName
+        );
+    }
+
+    removeParticipantByVariable (variableName) {
+        this._participants = this._participants.filter(
+            participant => participant.variableName !== variableName
+        );
+    }
 };
---
+++ tests/Map.test.js
@@ -0,0 +1,51 @@
+import {readFile, unlink, writeFile} from "fs/promises"
+import {resolve} from "path"
+import {describe, expect, it} from "vitest";
+
+import Map from "../src/Implementation/Map";
+
+describe("map tests", () => {
+
+    it("create map, set behavior and participants", async () => {
+        const mapPath = resolve(__dirname, "./test_data/TransactionDB_mapping.json");
+        const index = await readFile(mapPath, "utf-8");
+
+        for (const entry of JSON.parse(index)) {
+            const map = new Map(entry);
+            map.setBehavior("behavior1");
+            map.setParticipant("participant1", "variable1");
+            map.setParticipant("participant2", "variable2");
+            expect(map.getBehavior()).toEqual("behavior1");
+            expect(map.getParticipantByName("participant1"))
+                .toEqual({participantName: "participant1", variableName: "variable1"});
+            expect(map.getParticipantByVariable("variable2"))
+                .toEqual({participantName: "participant2", variableName: "variable2"});
+
+        }
+    });
+
+    it ("removes participants by name and variable", async () => {
+        const map = new Map({
+            uid: "map1",
+            start_line: 1,
+            end_line: 10,
+            source: "print('Hello, world!')",
+        });
+        map.setParticipant("participant1", "variable1");
+        map.setParticipant("participant2", "variable2");
+
+        // Remove participant by name and verify
+        map.removeParticipantByName("participant1");
+        expect(map.getParticipantByName("participant1")).toBeUndefined();
+        expect(map.getParticipants())
+            .toEqual([{ participantName: "participant2", variableName: "variable2" }]);
+
+        // Remove participant by variable and verify
+        map.removeParticipantByVariable("variable2");
+        expect(map.getParticipantByVariable("variable2")).toBeUndefined();
+        expect(map.getParticipants()).toEqual([]);
+
+        map.removeBehavior();
+        expect(map.getBehavior()).toBeNull();
+    });
+});
