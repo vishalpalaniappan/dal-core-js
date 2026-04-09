98573b26d19d86c6717150236df8aa264e99e8b4
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-09 13:31:37 -0400

    Add methods to implementation v2.

--- src/Implementation/ImplementationV2.js
+++ src/Implementation/ImplementationV2.js
@@ -1,3 +1,5 @@
+import File from "./File";
+
 /**
  * Note: I am migrating away from the existing implementation class in stages.
  * This class will eventually replace the existing implementation class.
@@ -9,6 +11,72 @@
  */
 export default class ImplementationV2 {
     constructor () {
+        this._files = [];
+    }
 
+    /**
+     * Add a source file to the implementation.
+     * @param {String} name
+     * @param {String} path
+     * @param {String} content
+     */
+    addFile (name, path, content) {
+        const f = new File(name, path);
+        f.addVersion();
+        f.addContent(content);
+        this._files.push(f);
+    }
+
+    /**
+     * Get the file with the given UID.
+     * @param {String} uid UID of the file to get.
+     * @returns {File} The file with the given UID.
+     * @throws {Error} Throws an error if a file with the given UID does not
+     * exist in the implementation.
+     */
+    getFile (uid) {
+        const file = this._files.find(file => file._uid === uid);
+        if (!file) {
+            throw new Error(`File with uid ${uid} not found in implementation.`);
+        }
+        return file;
+    }
+
+    /**
+     * Remove the file with the given UID from the implementation.
+     * @param {String} uid UID of the file to remove.
+     * @throws {Error} Throws an error if a file with the given UID does not
+     * exist in the implementation.
+     */
+    removeFile (uid) {
+        const fileIndex = this._files.findIndex(file => file._uid === uid);
+        if (fileIndex === -1) {
+            throw new Error(`File with uid ${uid} not found in implementation.`);
+        }
+        this._files.splice(fileIndex, 1);
+    }
+
+    /**
+     * Sets the statement index for a file in the implementation.
+     * @param {String} uid UID of the file.
+     * @param {Object} index Statement index to set for the file.
+     * @throws {Error} Throws an error if a file with the given UID does not
+     * exist in the implementation.
+     */
+    setStatementIndexForFile (uid, index) {
+        const file = this.getFile(uid);
+        file.addStatementIndex(index);
+    }
+
+    /**
+     * Gets the statement index for a file in the implementation.
+     * @param {String} uid UID of the file.
+     * @returns {Object} The statement index of the file.
+     * @throws {Error} Throws an error if a file with the given UID does not
+     * exist in the implementation.
+     */
+    getStatementIndexForFile (uid) {
+        const file = this.getFile(uid);
+        return file.getStatementIndex();
     }
 }
