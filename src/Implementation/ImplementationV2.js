522776d11f74163eaa81448fb5dd8ae722f34c8a
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-09 11:34:52 -0400

    Add v2 of implementation and add a note for migration plans and when PR will be merged.

---
+++ src/Implementation/ImplementationV2.js
@@ -0,0 +1,14 @@
+/**
+ * Note: I am migrating away from the existing implementation class in stages.
+ * This class will eventually replace the existing implementation class.
+ *
+ * The problem is I haven't merged the existing implementation but once I
+ * migrate to using this new implementation class, I will update the workbench
+ * to use this new implementation and then I will merge engine into main before
+ * I add any more features.
+ */
+export default class ImplementationV2 {
+    constructor () {
+
+    }
+}
