0e0bdee419ea2f8021e6887fa9aabd82f4eab769
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-13 10:21:44 -0400

    Remove outdated todo.

--- src/Implementation/Implementation.js
+++ src/Implementation/Implementation.js
@@ -1,15 +1,6 @@
 import Base from "../Base";
 import File from "./File";
 
-/**
- * Note: I am migrating away from the existing implementation class in stages.
- * This class will eventually replace the existing implementation class.
- *
- * The problem is I haven't merged the existing implementation but once I
- * migrate to using this new implementation class, I will update the workbench
- * to use this new implementation and then I will merge engine into main before
- * I add any more features.
- */
 export default class Implementation extends Base {
     constructor () {
         super();
