f22e08a14d03992b03942e8195ef911366c00ddc
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-16 13:03:04 -0400

    Create trace tests placeholder.

---
+++ tests/TracesTests/TracesTests.test.js
@@ -0,0 +1,13 @@
+import clpFfiJsModuleInit from "clp-ffi-js/node";
+import {readFile, unlink, writeFile} from "fs/promises"
+import {resolve} from "path"
+import {describe, expect, it} from "vitest";
+
+import {DALEngine} from "../../src/DALEngine.js";
+
+describe("traces tests", () => {
+
+    it("adds a trace", async () => {
+        
+    });
+});
