57fdd2c3eca6282311ae06f68f373f21620290ca
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-16 13:28:29 -0400

    Remove engine name and description because its meaningless.

--- tests/TracesTests/TracesTests.test.js
+++ tests/TracesTests/TracesTests.test.js
@@ -31,10 +31,7 @@ describe("traces tests", () => {
     });
 
     it("serializes engine with a trace and loads trace from file", async () => {
-        let d = new DALEngine({
-            name: "Execution Trace Walker",
-            description: "Walks through execution traces",
-        });
+        let d = new DALEngine({name: "", description: ""});
 
         const traceFilePath = resolve(
             __dirname, "../test_data/37901387-61a5-4a9c-980a-99bc9a6bd6ec.clp.zst"
@@ -49,7 +46,7 @@ describe("traces tests", () => {
         const tempFilePath = resolve(__dirname, "../temp/traceTemp.json")
         await writeFile(tempFilePath, d.serialize())
 
-        d = new DALEngine({name: "Library Manager", description: "Manages the library"});
+        d = new DALEngine({name: "", description: ""});
         d.deserialize(await readFile(tempFilePath));
 
         expect(d.traces.getTrace(traceUid).uid).toEqual(traceUid);
