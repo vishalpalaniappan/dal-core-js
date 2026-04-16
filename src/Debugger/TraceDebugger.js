49afac99576e9b12e4d9fdf1d8ecb208b6716e4b
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-16 07:28:46 -0400

    Remove unused arg.

--- src/Debugger/TraceDebugger.js
+++ src/Debugger/TraceDebugger.js
@@ -17,12 +17,10 @@ class TraceDebugger {
      *
      * @param {Object} design The design to use as the authoritative semantic
      * model for debugging.
-     * @param {Object} implementation The implementation to use for debugging.
      * @param {String} traceId The ID of the trace to debug.
      * @param {Array} traceLogs The logs of the trace to debug.
      */
-    constructor (design, implementation, traceId, traceLogs) {
-        this._implementation = implementation;
+    constructor (design, traceId, traceLogs) {
         this._traceId = traceId;
         this._design = design;
         this._logs = traceLogs.map((log) => new LogEntry(log));
