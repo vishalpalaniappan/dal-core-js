fbb659968e02355f82395130eeecfc14386a1d4a
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-19 11:56:02 -0400

    Add set primitive skeleton.

---
+++ src/BehaviorV2Test/SemanticPrimitives/SetPrimitive.js
@@ -0,0 +1,55 @@
+class SetPrimitive {
+    /**
+     * This primitive represents the semantic definition of the set operation.
+     * It defines the inputs, preconditions, transformation, and postconditions
+     * required to both compute the resulting state and verify the correctness
+     * of the transformation from the observed trace.
+     *
+     * The set primitive will have the follwing participants:
+     * - The target variable whose value is being set.
+     * - The key of the target variable that is being set
+     *      (if the target variable is a collection).
+     * - The value that is being set to the target variable.
+     *
+     * This semantic definition specifies that the provided value is assigned
+     * to the target variable. The expected postcondition is derived from the
+     * precondition and the transformation. This does not implement the set
+     * operation, but instead defines its semantic meaning and computes it.
+     * It is a declarative semantic rule that the engine can execute to derive
+     * the expected post-state, not an implementation of how the program
+     * performs the operation.
+     *
+     * Process:
+     * - The invariants of the precondition will be validated before the
+     * transformation is applied.
+     * - The transformation will set the value and then the post conditions
+     *   will be evaluated to check if the transformation is valid.
+     * - The invariants of the post conditions will be validated after the
+     * transformation is applied.
+     */
+    constructor () {
+        this._type = "set";
+    }
+
+    validate_inputs () {
+        // The inputs of the set primitive will be valiated here
+        // an the preconditions will be initialized.
+    }
+
+    process_preconditions () {
+        // Here the invariants of the preconditions will be
+        // validated.
+    }
+
+    apply_transformations () {
+        // Apply the transformations of the primitive.
+    }
+
+    process_postconditions () {
+        // After the transformations are applied, the post conditions
+        // will be evaluated for transformational validity and then
+        // the invariants of the post conditions will be validated.
+    }
+}
+
+export default SetPrimitive;
