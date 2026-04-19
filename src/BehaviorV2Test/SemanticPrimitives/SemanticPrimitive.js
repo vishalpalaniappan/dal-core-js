bc8549e3ddb8edac94d6f9023101ccb889107478
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-19 11:37:30 -0400

    Add base class for semantic primitive.

---
+++ src/BehaviorV2Test/SemanticPrimitives/SemanticPrimitive.js
@@ -0,0 +1,31 @@
+class SemanticPrimitive {
+    /**
+     * This is the base class that will be used for all
+     * semantic primitives. I am defining the common
+     * interface for all the primitives here but each
+     * primitive which extends this will have its own
+     * implementation of the methods.
+     * @param {String} name Name of primitive
+     */
+    constructor (name) {
+        this.name = name;
+    }
+
+    validate_inputs() {
+        // Valide the inputs of the primitive.
+    }
+
+    process_preconditions () {
+        // Process the preconditions of the primitive.
+    }
+
+    apply_transformations () {
+        // Apply the transformations of the primitive.
+    }
+
+    process_postconditions () {
+        // Proess the post conditions of the primitive.
+    }
+};
+
+export default SemanticPrimitive;
