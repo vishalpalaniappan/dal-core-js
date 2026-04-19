1266227382645049f47233cf289e4da18bd25e52
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-19 12:53:54 -0400

    Add set primitive test file to begin working through implementation.

---
+++ tests/PrimitiveTests/SetPrimitive/SetPrimitive.test.js
@@ -0,0 +1,11 @@
+
+import {describe, expect, it} from "vitest";
+
+import SetPrimitive from "../../../src/BehaviorV2Test/SemanticPrimitives/SetPrimitive.js"
+
+describe("tests the set primitive", () => {
+
+    it("tests adding inputs to set primitive", async () => {
+        const p = new SetPrimitive();
+    });
+});
