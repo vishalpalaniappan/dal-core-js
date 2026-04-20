e4ac1403dc7f982f7497dd75a421b4e5be0864d8
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-20 12:13:28 -0400

    Create two participanst to use for the instruction.

--- tests/ParserTests/ParserTests.test.js
+++ tests/ParserTests/ParserTests.test.js
@@ -1,10 +1,23 @@
 import {describe, expect, it} from "vitest";
-import BehavioralLanguageParser from "../../src/ExecutableModelTest/BehavioralLanguageParser.js";
 
+import BehavioralLanguageParser from "../../src/ExecutableModelTest/BehavioralLanguageParser.js";
+import Participant from "../../src/Design/Participant.js"
 
 describe("parser tests", () => {
 
     it("tests a simple parser script", async () => {
+        const p1 = new Participant({
+            name: "book",
+            description: "a book participant",
+        });
+        p1.setValue("BOOK1");
+
+        const p2 = new Participant({
+            name: "shelf",
+            description: "a shelf participant",
+        });
+        p2.setValue({});
+
         const parser = new BehavioralLanguageParser();
         parser.parse('set book shelf ["key1"]');
     });
