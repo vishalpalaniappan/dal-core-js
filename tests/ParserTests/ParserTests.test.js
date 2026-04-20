bfa1fa1b16a29a4ee134f7865760a37fe17c8787
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-20 12:05:49 -0400

    Update simple test.

--- tests/ParserTests/ParserTests.test.js
+++ tests/ParserTests/ParserTests.test.js
@@ -6,6 +6,6 @@ describe("parser tests", () => {
 
     it("tests a simple parser script", async () => {
         const parser = new BehavioralLanguageParser();
-        parser.parse('set book.location shelf ["key1","key2"]');
+        parser.parse('set book shelf ["key1"]');
     });
 });
