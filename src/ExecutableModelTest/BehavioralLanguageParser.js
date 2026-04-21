470227b1f19f5f2669be0f3945e3bdfb5d60161c
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-20 20:01:35 -0400

    Refactor behavior language parser to prepare for new primitives.

--- src/ExecutableModelTest/BehavioralLanguageParser.js
+++ src/ExecutableModelTest/BehavioralLanguageParser.js
@@ -1,6 +1,11 @@
 import InsertPrimitive from "./SemanticPrimitives/InsertPrimitive.js";
 import SetPrimitive from "./SemanticPrimitives/SetPrimitive.js";
 
+const re = {
+    "SET_RE": /^set\s+(.+?)\s+(.+?)(?:\s+(\[[^\]]*\]))?$/,
+    "INSERT_RE": /insert\s+(.+?)\s+(.+?)(?:\s+(\[[^\]]*\]))?\s+(.+)$/,    
+}
+
 class BehavioralLanguageParser {
     /**
      * Note: There is a more formal way to implement this, but I am
@@ -25,79 +30,42 @@ class BehavioralLanguageParser {
         this.primitiveConstructors = {};
     }
 
-    registerPrimitive (primitiveName, constructor) {
-        this.primitiveConstructors[primitiveName] = constructor;
-    }
-
     execute (script, participants, participants_post) {
         // Ex: set <target_participant> <value_participant> <key>
-        const SET_RE = /^set\s+(.+?)\s+(.+?)(?:\s+(\[[^\]]*\]))?$/;
-
-        const isSet = SET_RE.test(script);
+        const isSet = re["SET_RE"].test(script);
         if (isSet) {
-            const [, targetParticipantName, valueParticipantName, key] = script.match(SET_RE);
-            const updatedParticipants = this.executeSet(
-                targetParticipantName,
-                valueParticipantName,
-                JSON.parse(key)[0],
-                participants,
-                participants_post
-            );
-            return updatedParticipants;
+            return this.executeSet(script, participants,participants_post);
         }
 
         // Ex: insert <value> <target> [keys] <position>
-        const INSERT_RE = /insert\s+(.+?)\s+(.+?)(?:\s+(\[[^\]]*\]))?\s+(.+)$/;
-        const isInsert = INSERT_RE.test(script);
+        const isInsert = re["INSERT_RE"].test(script);
         if (isInsert) {
-            // eslint-disable-next-line max-len
-            const [, valueParticipantName, targetParticipantName, keys, position] = script.match(INSERT_RE);
-            const updatedParticipants = this.executeInsert(
-                valueParticipantName,
-                targetParticipantName,
-                JSON.parse(keys),
-                position,
-                participants,
-                participants_post
-            );
-            return updatedParticipants;
+            return this.executeInsert(script, participants);
         }
     }
 
-    executeSet (targetParticipantName, valueParticipantName, key, participants, participants_post) {
+    executeSet (script, participants) {
         console.log("Executing set");
+        const [, targetParticipantName, valueParticipantName, keys] = script.match(re["SET_RE"]);
         const input = {
-            key: key,
+            key: JSON.parse(keys)[0],
             targetParticipantName: targetParticipantName,
             valueParticipantName: valueParticipantName,
         };
-        const p = new SetPrimitive(input, participants, participants_post);
-        const output = p.apply_transformations();
-
-        // Update the value of the participants as set by the transformation
-        for (const key in participants) {
-            participants[key].setValue(output[key]._value);
-        }
-        return participants;
+        return new SetPrimitive(input, participants).apply_transformations();
     }
 
-    // eslint-disable-next-line max-len
-    executeInsert (valueParticipantName, targetParticipantName, keys, position, participants, participants_post) {
+    executeInsert (script, participants) {
         console.log( "Executing insert");
+        // eslint-disable-next-line max-len
+        const [, valueParticipantName, targetParticipantName, keys, position] = script.match(re["INSERT_RE"]);
         const input = {
             targetParticipantName: targetParticipantName,
-            key: keys[0],
+            key: JSON.parse(keys)[0],
             valueParticipantName: valueParticipantName,
             index: parseInt(position),
         };
-        const p = new InsertPrimitive(input, participants, participants_post);
-        const output = p.apply_transformations();
-
-        // Update the value of the participants as set by the transformation
-        for (const key in participants) {
-            participants[key].setValue(output[key]._value);
-        }
-        return participants;
+        return new InsertPrimitive(input, participants).apply_transformations();
     }
 }
 
