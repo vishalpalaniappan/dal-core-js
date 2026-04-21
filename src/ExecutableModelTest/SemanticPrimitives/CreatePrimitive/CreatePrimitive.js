c287158cad5d6644c83117aa82359b2f8061c27e
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-21 12:26:10 -0400

    Add create primitive to introduce participant into world.

---
+++ src/ExecutableModelTest/SemanticPrimitives/CreatePrimitive/CreatePrimitive.js
@@ -0,0 +1,50 @@
+import SemanticPrimitive from "../SemanticPrimitive.js";
+
+class CreatePrimitive extends SemanticPrimitive {
+    /**
+     * Semantic definition of create.
+     * Introduces a new participant into the world state.
+     *
+     * See README for details on how to use the primitive.
+     *
+     * @param {Object} inputs
+     * @param {Object} worldstate
+     */
+    constructor(inputs, worldstate) {
+        super("create");
+        this._type = "create";
+        this.validate_inputs(inputs);
+        this.worldState = worldstate;
+    }
+
+    validate_inputs(args) {
+        const expectedArgs = ["targetParticipantName"];
+        const missingKeys = expectedArgs.filter(key => !(key in args));
+
+        if (missingKeys.length > 0) {
+            throw new Error(`Missing required arguments: ${missingKeys.join(", ")}`);
+        }
+
+        this.targetParticipantName = args.targetParticipantName;
+        this.initialValue = args.initialValue ?? {};
+    }
+
+    apply_transformations() {
+        if (this.targetParticipantName in this.worldState) {
+            throw new Error(
+                `Participant "${this.targetParticipantName}" already exists in world state.`
+            );
+        }
+
+        const value =
+            typeof this.initialValue === "object"
+                ? structuredClone(this.initialValue)
+                : this.initialValue;
+
+        this.worldState[this.targetParticipantName] = value;
+
+        return this.worldState;
+    }
+}
+
+export default CreatePrimitive;
