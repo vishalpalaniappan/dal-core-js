b9d47b275eaaad813b55ad6d87f3cef30138055f
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-21 13:23:25 -0400

    Accept a book name.

--- tests/BehaviorSemanticExecutionTests/LibraryManagerBehaviorTests.test.js
+++ tests/BehaviorSemanticExecutionTests/LibraryManagerBehaviorTests.test.js
@@ -4,16 +4,29 @@ import ExecutableBehavior from "../../src/ExecutableModelTest/ExecutableBehavior
 
 describe("tests the behaviors in the library manager", () => {
 
-    it("tests the accept book behavior", async () => {
-        // Set the value of bookCopy to be book
-        const behavior = new ExecutableBehavior();
+    it("accepts a book", async () => {
+
+        // Creates a pending book
+        let behavior = new ExecutableBehavior();
         behavior.addPrimitive("create pendingBook");
         behavior.setPreWorldState({});
-        behavior.setPostWorldState({pendingBook: null});
-        const [updatedParticipants, isValid] = behavior.computeTransformations({
-            pendingBook: null,
-        });
+        behavior.setPostWorldState({pendingBook: {}});
+        behavior.setArgs({initialValue: {}});
+        let [updatedParticipants, isValid] = behavior.computeTransformations();
         expect(isValid).toBe(true);
-        expect(updatedParticipants.pendingBook).toBe(null);
+        expect(updatedParticipants.pendingBook).toEqual({});
+
+
+        // Accepts a book name
+        behavior = new ExecutableBehavior();
+        behavior.addPrimitive("create tempName");
+        behavior.addPrimitive('set pendingBook tempName ["name"]');
+        behavior.addPrimitive("remove tempName");
+        behavior.setArgs({initialValue: "Harry Potter"});
+        behavior.setPreWorldState(updatedParticipants);
+        behavior.setPostWorldState({pendingBook: {"name": "Harry Potter"}});
+        [updatedParticipants, isValid] = behavior.computeTransformations();
+        expect(isValid).toBe(true);
+        expect(updatedParticipants.pendingBook).toEqual({"name": "Harry Potter"});
     });
 });
