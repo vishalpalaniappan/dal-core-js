97549015888a621ed223bdb735e038fbee00da21
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-05-04 15:32:01 -0400

    Add note indicating I haven't migrated to using trace class yet.

--- src/Debugger/Trace.js
+++ src/Debugger/Trace.js
@@ -1,6 +1,12 @@
+/**
+ * Not used currently:
+ * I am just storing it in an object in the traces class, but I should
+ * move to using this class so everything is more maintainable.
+ */
+
 export default class Trace {
     /**
-     * Crated with an instance of the execution trace.
+     * Created with an instance of the execution trace.
      * When this object is created, the trace will be
      * automatically debugged using the trace debugger
      * and the results will be available for consuming
