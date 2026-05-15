5e283c8219d84470f443dc3bd3b048c0df66ca79
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-05-15 08:43:01 -0400

    Save primitive invokation method in object to avoid manually processing each primitive.

--- src/Design/BehavioralLanguage/PRIMITIVE_REGISTRY.js
+++ src/Design/BehavioralLanguage/PRIMITIVE_REGISTRY.js
@@ -1,3 +1,18 @@
+/* eslint-disable max-len */
+import RequirePrimitive from "./Context/RequirePrimitive/RequirePrimitive.js";
+import SelectPrimitive from "./Context/SelectPrimitive/SelectPrimitive.js";
+import InvariantParser from "./Invariants/InvariantParser.js";
+import CreatePrimitive from "./SemanticPrimitives/CreatePrimitive/CreatePrimitive.js";
+import GetFromPosPrimitive from "./SemanticPrimitives/GetFromPosPrimitive/GetFromPosPrimitive.js";
+import GetLengthPrimitive from "./SemanticPrimitives/GetLengthPrimitive/GetLengthPrimitive.js";
+import GetPrimitive from "./SemanticPrimitives/GetPrimitive/GetPrimitive.js";
+import HasKeyPrimitive from "./SemanticPrimitives/HasKeyPrimitive/HasKeyPrimitive.js";
+import InsertPrimitive from "./SemanticPrimitives/InsertPrimitive/InsertPrimitive.js";
+import IsEqualPrimitive from "./SemanticPrimitives/IsEqualPrimitive/IsEqualPrimitive.js";
+import RemoveFromPositionPrimitive from "./SemanticPrimitives/RemoveFromPosPrimitive/RemoveFromPosPrimitive.js";
+import RemovePrimitive from "./SemanticPrimitives/RemovePrimitive/RemovePrimitive.js";
+import SetPrimitive from "./SemanticPrimitives/SetPrimitive/SetPrimitive.js";
+
 export const PRIMITIVE_REGISTRY = Object.freeze({
     "SET_RE": /^set\s+(.+?)\s+(.+?)(?:\s+(\[[^\]]*\]))?$/,
     "INSERT_RE": /insert\s+(.+?)\s+(.+?)(?:\s+(\[[^\]]*\]))?\s+(.+)$/,
@@ -14,3 +29,63 @@ export const PRIMITIVE_REGISTRY = Object.freeze({
     "GET_LENGTH_RE": /^getLength\s+(\w+)\s+(\w+)$/,
     "SELECT_RE": /^select\s+(\w+)(?:\s+if\s+(\w+))?$/,
 });
+
+export const PRIMITIVE_BINDINGS = Object.freeze({
+    "SET": {
+        "invoke": SetPrimitive,
+        "regex": PRIMITIVE_REGISTRY.SET_RE,
+    },
+    "INSERT": {
+        "invoke": InsertPrimitive,
+        "regex": PRIMITIVE_REGISTRY.INSERT_RE,
+    },
+    "GET": {
+        "invoke": GetPrimitive,
+        "regex": PRIMITIVE_REGISTRY.GET_RE,
+    },
+    "REMOVE_KEY": {
+        "invoke": RemovePrimitive,
+        "regex": PRIMITIVE_REGISTRY.REMOVE_KEY_RE,
+    },
+    "CREATE": {
+        "invoke": CreatePrimitive,
+        "regex": PRIMITIVE_REGISTRY.CREATE_RE,
+    },
+    "REMOVE": {
+        "invoke": RemovePrimitive,
+        "regex": PRIMITIVE_REGISTRY.REMOVE_RE,
+    },
+    "GET_FROM_POS": {
+        "invoke": GetFromPosPrimitive,
+        "regex": PRIMITIVE_REGISTRY.GET_FROM_POS_RE,
+    },
+    "REMOVE_FROM_POS": {
+        "invoke": RemoveFromPositionPrimitive,
+        "regex": PRIMITIVE_REGISTRY.REMOVE_FROM_POS_RE,
+    },
+    "HAS_KEY": {
+        "invoke": HasKeyPrimitive,
+        "regex": PRIMITIVE_REGISTRY.HAS_KEY_RE,
+    },
+    "REQUIRE": {
+        "invoke": RequirePrimitive,
+        "regex": PRIMITIVE_REGISTRY.REQUIRE_RE,
+    },
+    "INVARIANT": {
+        "invoke": InvariantParser,
+        "regex": PRIMITIVE_REGISTRY.INVARIANT_RE,
+    },
+    "IS_EQUAL": {
+        "invoke": IsEqualPrimitive,
+        "regex": PRIMITIVE_REGISTRY.IS_EQUAL_RE,
+    },
+    "GET_LENGTH": {
+        "invoke": GetLengthPrimitive,
+        "regex": PRIMITIVE_REGISTRY.GET_LENGTH_RE,
+    },
+    "SELECT": {
+        "invoke": SelectPrimitive,
+        "regex": PRIMITIVE_REGISTRY.SELECT_RE,
+    },
+});
+
