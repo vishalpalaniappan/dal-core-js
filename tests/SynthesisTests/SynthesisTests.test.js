dea5ecb522f6a3ccf30c74f8a1be7f698db12531
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-05-17 16:41:35 -0400

    Add synthesis test file to begin testing process.

---
+++ tests/SynthesisTests/SynthesisTests.test.js
@@ -0,0 +1,11 @@
+import {writeFile} from "fs/promises"
+import {resolve} from "path"
+import {describe, expect, it} from "vitest";
+
+describe("tests synthesis meta output", () => {
+
+    it("tests get primitive synthesis meta", async () => {
+
+    });
+
+});
