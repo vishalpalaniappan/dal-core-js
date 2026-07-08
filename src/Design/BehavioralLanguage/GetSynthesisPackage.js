2cc48bcdb56e2a7a9803b35170b28e3c278b3798
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-07-07 22:16:40 -0400

    Remove TODO (will revisit it later).

--- src/Design/BehavioralLanguage/GetSynthesisPackage.js
+++ src/Design/BehavioralLanguage/GetSynthesisPackage.js
@@ -8,16 +8,6 @@ export const GetSynthesisPackage = (script) => {
      * used to synthesize the behavior in the target language. Currently that
      * language is in python and the actual synthesis methods are implemented in
      * a separate tool. In the future, it will be integrated into the engine.
-     *
-     * TODO: I want to create a mechanism to allow the developer to register
-     * effects with the engine. These effects will then become part of the
-     * synthesis package and will be used in the transformations. I feel that
-     * this is a clean way to integrate the scaffolding needed to enable the
-     * design to interact with reality. I will explore this solution more as I
-     * implement it but the core idea makes sense to me, rather than invoke the
-     * behavior from the substrate level mechs, I let the behavioe invoke the
-     * substrate level mechs through the effects and the effects can be swapped
-     * out as needed for different substrates.
      */
 
 
