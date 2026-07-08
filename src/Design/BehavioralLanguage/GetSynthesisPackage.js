7fedb60af88cec0c0530c2d1f5b4e681c1f2b92f
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-07-07 22:42:48 -0400

    Add note/TODO regarding planning while exploring.

--- src/Design/BehavioralLanguage/GetSynthesisPackage.js
+++ src/Design/BehavioralLanguage/GetSynthesisPackage.js
@@ -20,7 +20,23 @@ export const GetSynthesisPackage = (script) => {
 
         let executionOutput;
         try {
-            // Worldstate null to only get the synthesis meta
+            /**
+             * TODO:
+             * I am setting world state to null to indicate to the
+             * behavioral language parser that I only want the synthesis
+             * meta. This is getting convoluted because I am making changes
+             * building on things I want to change. Soon, once I have a working
+             * workflow for the synthesis, I will return to this and rewrite
+             * this in a very simple way. This is because once synthesis is
+             * working, there is no more applying transformation in the
+             * engine, the synthesized behavior is what is executed, so it
+             * will become very simple.
+             *
+             * The main issue is, I am in the exploration phase and its
+             * hard to write well planned code (because the plan keeps changing)
+             * I am just prioritizing prove out the conclusions of my thoughts,
+             * and then I will build cleanly with everything I've learned.
+             */
             const worldState = null;
             executionOutput = this.BehavioralLanguageParser.execute(
                 line, worldState, this.args
